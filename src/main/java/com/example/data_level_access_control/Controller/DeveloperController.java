fcfe432f75e8e817c51aea3e6f23b3fdb888af92
Phaneendra Marisa <phaneendra.marisa@paysafe.com>
2025-04-21 22:20:47 +0530

    Added dev controller.

--- src/main/java/com/example/data_level_access_control/Controller/DeveloperController.java
+++ src/main/java/com/example/data_level_access_control/Controller/DeveloperController.java
@@ -1,8 +1,5 @@
 package com.example.data_level_access_control.Controller;
 
-import com.example.data_level_access_control.model.Merchant;
-import com.example.data_level_access_control.repository.MerchantRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DeveloperController {
-
-  private final MerchantRepository merchantRepository;
-
-  public DeveloperController(MerchantRepository merchantRepository) {
-    this.merchantRepository = merchantRepository;
-  }
 
   @GetMapping("/ping")
   public ResponseEntity<String> getAllTutorials() {
