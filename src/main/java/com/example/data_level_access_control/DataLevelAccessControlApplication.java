256f9c9ab3e5ec4e538da742950e9000b1605c3c
Phaneendra Marisa <phaneendra.marisa@paysafe.com>
2025-04-21 19:16:07 +0530

    Initial commit.

---
+++ src/main/java/com/example/data_level_access_control/DataLevelAccessControlApplication.java
@@ -0,0 +1,13 @@
+package com.example.data_level_access_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataLevelAccessControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataLevelAccessControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/data_level_access_control/DataLevelAccessControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.data_level_access_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataLevelAccessControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
